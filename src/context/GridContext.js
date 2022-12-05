fc8e611820d2e3d9d7672778d3304064063cff25
BGMloop <gabrielmaele1@yahoo.com>
2022-12-05 15:52:44 -0600

    The grid, the cells, and the useContext

---
+++ src/context/GridContext.js
@@ -0,0 +1,17 @@
+import { useContext,createContext } from "react";
+
+const context = createContext()
+
+export const useParams=()=>{
+    return useContext(context)
+}
+
+export const ParamsProvider = ({children}) => {
+
+      return (<div>
+       <context.Provider value={1500}>
+        {children}
+       </context.Provider>
+      </div>)
+
+}
